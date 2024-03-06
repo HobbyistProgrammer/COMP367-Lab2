9f76f2276747f11982e335c30a4b8a0b645b8de7
Benton Le <ble21@my.centennialcollege.ca>
2024-03-05 19:28:52 -0500

    Changes to Greetings

---
+++ COMP367_Lab2/src/test/java/com/comp303/assignment1/BentonLeComp303Assignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp303.assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BentonLeComp303Assignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
